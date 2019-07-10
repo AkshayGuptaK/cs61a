11f5623ca639fc0ab6a9a214049e68a829ae8e3b
Akshay Gupta <akshaygrkesh@gmail.com>
2019-07-10 21:24:16 +0530

    Updated other materials

--- getScripts.sh
+++ getScripts.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in {10..37}
+for i in {32..35}
 do
-    curl https://inst.eecs.berkeley.edu/~cs61a/fa18/assets/slides/$i.py -o $i.py
+    curl https://inst.eecs.berkeley.edu/~cs61a/fa18/assets/slides/$i.sql -o $i.sql
 done
